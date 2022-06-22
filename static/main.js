f998bfece16ae4dd48d22dedea2c3dc71161a516
LilaSarfson <lilamandarina@hotmail.com>
2022-06-22 13:31:08 +0200

    cambia el dropbox

--- static/main.js
+++ static/main.js
@@ -1,83 +1,123 @@
-let dropBox= document.querySelector(".dropBox");
+let dropBox = document.querySelector(".dropBox");
 let button = dropBox.querySelector("button");
 let input = dropBox.querySelector("input");
 let dropZone = document.querySelector(".dropZone");
 let documentZone1 = document.querySelector(".newDoc");
 FileList = [];
-button.addEventListener("click", (event)=> {
-event.preventDefault();
-clickOnInput();
-console.log(input.files)
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  clickOnInput();
+  console.log(input.files)
 });
 
-    dropBox.addEventListener('dragover',  dragOverHandler, false)
-    dropBox.addEventListener('drop',  dropHandler, false)
+function clickOnInput() {
 
-function clickOnInput(){
-    
-    input.click();
- 
-        };
+  input.click();
 
-        // Añadir un archivo con el botón
+};
 
-input.addEventListener("change", (e)=>{
-    files = input.files;
-    // if(files[0].type == "image/png" || files[0].type == "image/jpg"){
-    //     let src= document.querySelector(".iconoDoc").src;
-    //     src == "./Image/pictures.png";
-    // }
-    documentZone1.innerHTML += `
+// Añadir un archivo con el botón
+
+input.addEventListener("change", (e) => {
+  files = input.files;
+  documentZone1.innerHTML += `
     <div class="documentZone">
-    <div class="docDownloaded"><img class="iconoDoc" src="./Image/${files[0].type}.png" alt=""></div>
-    <div class="uploadZone">
-        <h3>${files[0].name}</h3>
-        <div style="width: 50%" class="Upload">
-            <div>80 %</div>
-        </div>
-    </div>
-</div>
+                        <div class="docDownloaded"><img class="iconoDoc" src="./Image/pictures.png" alt=""></div>
+                        <div class="uploadZone">
+                            <h3 class="fileName">NombredeArchivo1</h3>
+                            <div style="width: 50%" class="Upload">
+                                <div>80 %</div>
+                            </div>
+                        </div>
+                    </div>
     `
- fileList = fileList + files;
- console.log(fileList)
+  fileList = fileList + files;
+  console.log(fileList)
 })
 
 // Añadir un archivo dropeandolo
 
+dropBox.addEventListener('dragover', dragOverHandler, false)
+dropBox.addEventListener('drop', dropHandler, false)
+
 function dropHandler(ev) {
-    console.log('File(s) dropped');
-    ev.preventDefault();
-  
-    if (ev.dataTransfer.items) {
-      // Use DataTransferItemList interface to access the file(s)
-      for (let i = 0; i < ev.dataTransfer.items.length; i++) {
-          multipleFile =  ev.dataTransfer.files[i]
-          console.log(multipleFile);
-        // If dropped items aren't files, reject them
-        if (ev.dataTransfer.items[i].kind === 'file') {
-          const file = ev.dataTransfer.items[i].getAsFile();
-        }
+  console.log('File(s) dropped');
+  ev.preventDefault();
+  if (ev.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    for (let i = 0; i < ev.dataTransfer.items.length; i++) {
+      multipleFile = ev.dataTransfer.files[i]
+      console.log(multipleFile);
+      // If dropped items aren't files, reject them
+      if (ev.dataTransfer.items[i].kind === 'file') {
+        const file = ev.dataTransfer.items[i].getAsFile();
       }
-    } else {
-      // Use DataTransfer interface to access the file(s)
-      for (let i = 0; i < ev.dataTransfer.files.length; i++) {
-      }
- }
-    documentZone1.innerHTML += `
-    <div class="documentZone">
+    }
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    for (let i = 0; i < ev.dataTransfer.files.length; i++) {
+    }
+  }
+  documentZone1.innerHTML += `
+  <div class="documentZone">
     <div class="docDownloaded"><img class="iconoDoc"  alt=""></div>
     <div class="uploadZone">
-        <h3>${multipleFile.name}</h3>
+    <h3 class="fileName">${multipleFile.name}</h3>
         <div style="width: 50%" class="Upload">
-            <div>80 %</div>
+        <div>80 %</div>
         </div>
-    </div>
-</div>
-    `
+        </div>
+        </div>
+        `
+}
+function dragOverHandler(ev) {
+  ev.preventDefault();
+}
+
+dropBox.addEventListener("dragenter", event => {
+  // highlight potential drop target when the draggable element enters it
+  if (event.target.classList.contains("dropzone")) {
+    event.target.classList.add("dragover");
   }
-  function dragOverHandler(ev) {
-    ev.preventDefault();
+  document.querySelector(".droptext p").textContent = "Suelta aquí tu archivo";
+  event.preventDefault();
+});
+
+dropBox.addEventListener("dragleave", event => {
+  // reset background of potential drop target when the draggable element leaves it
+  if (event.target.classList.contains("dropzone")) {
+    event.target.classList.remove("dragover");
   }
+  document.querySelector(".droptext p").textContent = "Puedes dropear tu archivo aquí o"
+  event.preventDefault();
+});
+
+// let textbox = document.querySelector(".droptext");
+
+// textbox.addEventListener("dragenter", event => {
+//   // highlight potential drop target when the draggable element enters it
+//   if (event.target.classList.contains("dropzone")) {
+//     event.target.classList.add("dragover");
+//   }
+//   document.querySelector(".droptext p").textContent = "Suelta aquí tu archivo";
+//   event.preventDefault();
+// });
+
+
+
+// textbox.addEventListener("dragleave", event => {
+//   // reset background of potential drop target when the draggable element leaves it
+//   if (event.target.classList.contains("dropzone")) {
+//     event.target.classList.remove("dragover");
+//     console.log("funcionoo")
+//   }
+//   document.querySelector(".droptext p").textContent = "Puedes dropear tu archivo aquí o"
+//   event.preventDefault();
+// });
+
+
+
 
 //  GOOGLE AUTHENTICATOR
 
@@ -89,91 +129,91 @@ const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/res
 
 const SCOPES = 'https://www.googleapis.com/drive.file';
 let tokenClient;
-      let gapiInited = false;
-      let gisInited = false;
+let gapiInited = false;
+let gisInited = false;
 
-      document.getElementById('authorize_button').style.visibility = 'hidden';
-      document.getElementById('signout_button').style.visibility = 'hidden';
+document.getElementById('authorize_button').style.visibility = 'hidden';
+document.getElementById('signout_button').style.visibility = 'hidden';
 
-      /**
-       * Callback after api.js is loaded.
-       */
-      function gapiLoaded() {
-        gapi.load('client', intializeGapiClient);
-      }
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
+  gapi.load('client', intializeGapiClient);
+}
 
-      /**
-       * Callback after the API client is loaded. Loads the
-       * discovery doc to initialize the API.
-       */
-      async function intializeGapiClient() {
-        await gapi.client.init({
-          apiKey: API_KEY,
-          discoveryDocs: [DISCOVERY_DOC],
-        });
-        gapiInited = true;
-        maybeEnableButtons();
-      }
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
+async function intializeGapiClient() {
+  await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: [DISCOVERY_DOC],
+  });
+  gapiInited = true;
+  maybeEnableButtons();
+}
 
-      /**
-       * Callback after Google Identity Services are loaded.
-       */
-      function gisLoaded() {
-        tokenClient = google.accounts.oauth2.initTokenClient({
-          client_id: CLIENT_ID,
-          scope: SCOPES,
-          callback: '', // defined later
-        });
-        gisInited = true;
-        maybeEnableButtons();
-      }
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: '', // defined later
+  });
+  gisInited = true;
+  maybeEnableButtons();
+}
 
-      /**
-       * Enables user interaction after all libraries are loaded.
-       */
-      function maybeEnableButtons() {
-        if (gapiInited && gisInited) {
-          document.getElementById('authorize_button').style.visibility = 'visible';
-        }
-      }
+/**
+ * Enables user interaction after all libraries are loaded.
+ */
+function maybeEnableButtons() {
+  if (gapiInited && gisInited) {
+    document.getElementById('authorize_button').style.visibility = 'visible';
+  }
+}
 
-      /**
-       *  Sign in the user upon button click.
-       */
-      function handleAuthClick() {
-        tokenClient.callback = async (resp) => {
-          if (resp.error !== undefined) {
-            throw (resp);
-          }
-          document.getElementById('signout_button').style.visibility = 'visible';
-          document.getElementById('authorize_button').innerText = 'Refresh';
-          await listFiles();
-        };
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick() {
+  tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+      throw (resp);
+    }
+    document.getElementById('signout_button').style.visibility = 'visible';
+    document.getElementById('authorize_button').innerText = 'Refresh';
+    await listFiles();
+  };
 
-        if (gapi.client.getToken() === null) {
-          // Prompt the user to select a Google Account and ask for consent to share their data
-          // when establishing a new session.
-          tokenClient.requestAccessToken({prompt: 'consent'});
-        } else {
-          // Skip display of account chooser and consent dialog for an existing session.
-          tokenClient.requestAccessToken({prompt: ''});
-        }
-      }
-      function handleSignoutClick() {
-        const token = gapi.client.getToken();
-        if (token !== null) {
-          google.accounts.oauth2.revoke(token.access_token);
-          gapi.client.setToken('');
-          document.getElementById('content').innerText = '';
-          document.getElementById('authorize_button').innerText = 'Authorize';
-          document.getElementById('signout_button').style.visibility = 'hidden';
-        }
-      }
+  if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({ prompt: 'consent' });
+  } else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({ prompt: '' });
+  }
+}
+function handleSignoutClick() {
+  const token = gapi.client.getToken();
+  if (token !== null) {
+    google.accounts.oauth2.revoke(token.access_token);
+    gapi.client.setToken('');
+    document.getElementById('content').innerText = '';
+    document.getElementById('authorize_button').innerText = 'Authorize';
+    document.getElementById('signout_button').style.visibility = 'hidden';
+  }
+}
 
-      function onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-      }
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
